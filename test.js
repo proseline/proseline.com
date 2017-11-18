d3ce9c40ccc95b130ccac65ad3e0478522e00fb5
Kyle E. Mitchell <kyle@kemitchell.com>
2017-11-17 20:58:24 -0800

    Add draft, note, and marker schemas

--- test.js
+++ test.js
@@ -1,8 +1,20 @@
 var AJV = require('ajv')
 var assert = require('assert')
-var documentSchema = require('./schemas/document')
 
 var ajv = new AJV()
 
+var documentSchema = require('./schemas/document')
 ajv.validateSchema(documentSchema)
 assert.deepEqual(ajv.errors, null, 'document schema')
+
+var markerSchema = require('./schemas/marker')
+ajv.validateSchema(markerSchema)
+assert.deepEqual(ajv.errors, null, 'marker schema')
+
+var noteSchema = require('./schemas/note')
+ajv.validateSchema(noteSchema)
+assert.deepEqual(ajv.errors, null, 'note schema')
+
+var draftSchema = require('./schemas/draft')
+ajv.validateSchema(draftSchema)
+assert.deepEqual(ajv.errors, null, 'draft schema')
