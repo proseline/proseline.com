cba7530ca8c66fda03879855596c49090a36258b
Kyle E. Mitchell <kyle@kemitchell.com>
2018-02-20 19:13:28 -0800

    Improve development server

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 var http = require('http')
+var fs = require('fs')
 
-var ecstatic = require('ecstatic')({
-  root: 'build',
-  cache: 0
-})
-
-http
-  .createServer(function (request, response) {
-    ecstatic(request, response, {handleError: false})
+http.createServer()
+  .on('request', function (request, response) {
+    var url = request.url
+    if (url === '/styles.css') {
+      response.setHeader('Content-Type', 'text/css')
+      send('build/styles.css')
+    } else if (url === '/styles.css.map') {
+      send('build/styles.css.map')
+    } else if (url === '/browser.js') {
+      response.setHeader('Content-Type', 'application/json')
+      send('build/browser.js')
+    } else if (url === '/browser.js.map') {
+      send('build/browser.js.map')
+    } else {
+      response.setHeader('Content-Type', 'text/html')
+      send('build/index.html')
+    }
+    function send (path) {
+      fs.createReadStream(path).pipe(response)
+    }
   })
   .listen(process.env.PORT || 8000)
