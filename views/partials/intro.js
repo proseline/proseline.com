eb6697bb7c790c2b85f1f0f3126832c1ff709fcc
Kyle E. Mitchell <kyle@kemitchell.com>
2018-02-21 17:21:12 -0800

    Show "You" even without intro

--- views/partials/intro.js
+++ views/partials/intro.js
@@ -2,17 +2,15 @@ module.exports = function (state, publicKey) {
   var span = document.createElement('span')
   span.className = 'intro'
   var intro = state.intros[publicKey]
-  if (intro) {
-    if (intro.publicKey === state.identity.publicKey) {
-      span.appendChild(document.createTextNode('You'))
-    } else {
-      span.appendChild(
-        document.createTextNode(
-          intro.message.body.name +
-          ' (on ' + intro.message.body.device + ')'
-        )
+  if (publicKey === state.identity.publicKey) {
+    span.appendChild(document.createTextNode('You'))
+  } else if (intro) {
+    span.appendChild(
+      document.createTextNode(
+        intro.message.body.name +
+        ' (on ' + intro.message.body.device + ')'
       )
-    }
+    )
   } else {
     span.appendChild(document.createTextNode(publicKey))
   }
