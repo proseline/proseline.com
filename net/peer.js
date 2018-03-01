cc8726e3f334e3eed41bcb10b841abb4ed091ed5
Kyle E. Mitchell <kyle@kemitchell.com>
2018-02-28 20:45:45 -0800

    Use WebRTC configuration from iceservers.proseline.com

--- net/peer.js
+++ net/peer.js
@@ -3,6 +3,7 @@ var assert = require('assert')
 var debug = require('debug')('proseline:peer')
 var replicate = require('./replicate')
 var signalhub = require('signalhub')
+var simpleGet = require('simple-get')
 var webRTCSwarm = require('webrtc-swarm')
 
 // TODO: tune maxPeers by last access time
@@ -18,23 +19,29 @@ function joinSwarm (project, database) {
     return swarm.project.discoveryKey === project.discoveryKey
   })
   if (alreadyJoined) return
-  var hub = signalhub('proseline-' + project.discoveryKey, HUBS)
-  var swarm = webRTCSwarm(hub, {maxPeers: 3})
-  swarm.on('peer', function (peer, id) {
-    debug('peer: %o', id)
-    var replicationStream = replicate({
-      secretKey: project.secretKey,
-      discoveryKey: project.discoveryKey,
-      database: database
+  simpleGet.concat({
+    url: 'https://iceservers.proseline.com/_servers',
+    timeout: 6000
+  }, function (error, response, data) {
+    var options = {maxPeers: 3}
+    if (!error) options.config = data
+    var hub = signalhub('proseline-' + project.discoveryKey, HUBS)
+    var swarm = webRTCSwarm(hub, options)
+    swarm.on('peer', function (peer, id) {
+      debug('peer: %o', id)
+      var replicationStream = replicate({
+        secretKey: project.secretKey,
+        discoveryKey: project.discoveryKey,
+        database: database
+      })
+      replicationStream.pipe(peer).pipe(replicationStream)
     })
-    replicationStream.pipe(peer).pipe(replicationStream)
+    swarms.push({
+      project: project,
+      swarm: swarm
+    })
+    debug('joined: %s', project.discoveryKey)
   })
-  swarms.push({
-    project: project,
-    swarm: swarm
-  })
-  debug('joined: %s', project.discoveryKey)
-  return swarm
 }
 
 function leaveSwarm (discoveryKey) {
