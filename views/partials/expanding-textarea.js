8f04b53c1b059998046e07533434edca788be9d9
Kyle E. Mitchell <kyle@kemitchell.com>
2018-03-15 02:26:41 -0700

    Refactor and add comments

--- views/partials/expanding-textarea.js
+++
@@ -1,5 +0,0 @@
-module.exports = function () {
-  var textarea = document.createElement('textarea')
-  textarea.rows = 3
-  return textarea
-}
--- views/viewer.js
+++ views/viewer.js
@@ -1,7 +1,6 @@
 var assert = require('assert')
 var initializeEditor = require('../editor')
 var renderDraftHeader = require('./partials/draft-header')
-var renderExpandingTextArea = require('./partials/expanding-textarea')
 var renderIntro = require('./partials/intro')
 var renderLoading = require('./loading')
 var renderRefreshNotice = require('./partials/refresh-notice')
@@ -10,6 +9,7 @@ var withProject = require('./with-project')
 
 module.exports = withProject(function (state, send, discoveryKey, digest) {
   state.route = 'viewer'
+  // <main>
   var main = document.createElement('main')
   if (state.draft && state.draft.digest === digest) {
     if (state.changed) {
@@ -51,9 +51,9 @@ module.exports = withProject(function (state, send, discoveryKey, digest) {
 var SAVE_FORM_CLASS = 'saveDraftForm'
 
 function renderSaveForm (state, send, editor) {
+  // <form>
   var form = document.createElement('form')
   form.className = SAVE_FORM_CLASS + ' hidden'
-
   form.addEventListener('submit', function (event) {
     event.preventDefault()
     event.stopPropagation()
@@ -64,7 +64,7 @@ function renderSaveForm (state, send, editor) {
     })
   })
 
-  // Save Button
+  // <button>
   var save = document.createElement('button')
   form.appendChild(save)
   save.className = 'button'
@@ -88,6 +88,7 @@ function renderText (text) {
 }
 
 function renderMarkForm (state, send) {
+  // <form>
   var form = document.createElement('form')
   form.id = 'markDraft'
   form.addEventListener('submit', function (event) {
@@ -105,6 +106,7 @@ function renderMarkForm (state, send) {
     })
   })
 
+  // <input>
   var input = document.createElement('input')
   input.required = true
   form.appendChild(input)
@@ -116,6 +118,7 @@ function renderMarkForm (state, send) {
     )
   })
   if (marksICanMove.length !== 0) {
+    // <datalist>
     var datalist = document.createElement('datalist')
     form.appendChild(datalist)
     datalist.id = 'marksICanMove'
@@ -127,6 +130,7 @@ function renderMarkForm (state, send) {
     })
   }
 
+  // <button>
   var button = document.createElement('button')
   button.type = 'submit'
   button.appendChild(document.createTextNode('Put a mark on this draft.'))
@@ -136,10 +140,12 @@ function renderMarkForm (state, send) {
 }
 
 function renderNote (state, send, note) {
+  // <aside>
   var aside = document.createElement('aside')
   aside.className = 'note'
   aside.id = note.digest
   var replyTo = state.replyTo
+
   // <p>
   var p = document.createElement('p')
   p.className = 'byline'
@@ -148,28 +154,33 @@ function renderNote (state, send, note) {
   p.appendChild(renderRelativeTimestamp(note.message.body.timestamp))
   p.appendChild(document.createTextNode(':'))
   aside.appendChild(p)
+
   // <blockquote>
   var blockquote = document.createElement('blockquote')
-  blockquote.appendChild(renderText(note.message.body.text))
   aside.appendChild(blockquote)
+  blockquote.appendChild(renderText(note.message.body.text))
+
   if (replyTo === note.digest) {
     aside.appendChild(renderNoteForm(state, send, {parent: note.digest}))
   } else {
     // <button>
     var button = document.createElement('button')
+    aside.appendChild(button)
     button.addEventListener('click', function () {
       send('reply to', note.digest)
     })
     button.appendChild(document.createTextNode('Reply to this note.'))
-    aside.appendChild(button)
   }
+
   if (note.children.length !== 0) {
+    // <ol>
     var ol = document.createElement('ol')
     note.children.forEach(function (child) {
       ol.appendChild(renderNote(state, send, child))
     })
     aside.appendChild(ol)
   }
+
   return aside
 }
 
@@ -190,30 +201,32 @@ function renderNoteForm (state, send, options) {
   )
   assert(!selected || typeof selected === 'string')
   assert.equal(typeof send, 'function')
+
+  // <form>
   var form = document.createElement('form')
   form.className = 'noteForm'
   form.addEventListener('submit', function (event) {
     event.preventDefault()
     event.stopPropagation()
-    send('note', {
-      parent,
-      range,
-      text: textarea.value
-    })
+    send('note', {parent, range, text: textarea.value})
   })
+
   // <textarea>
-  var textarea = renderExpandingTextArea()
-  textarea.required = true
-  textarea.autofocus = false
+  var textarea = document.createElement('textarea')
   form.appendChild(textarea)
+  textarea.required = true
+  textarea.rows = 3
+  textarea.autofocus = false
+
   // <button>
   var button = document.createElement('button')
+  form.appendChild(button)
   button.type = 'submit'
   button.appendChild(
     document.createTextNode(
       parent ? 'Add your reply.' : 'Add your note.'
     )
   )
-  form.appendChild(button)
+
   return form
 }
