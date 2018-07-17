a93b53bf65b940da2be5d0cb8472f7a7abd36705
Kyle E. Mitchell <kyle@kemitchell.com>
2018-07-17 14:12:26 -0700

    Fix display of Stripe errors

--- views/partials/payment.js
+++ views/partials/payment.js
@@ -3,15 +3,19 @@ var stripe = require('../../stripe')
 module.exports = function () {
   var elements = stripe.elements()
   var card = elements.create('card')
-  var returned = document.createElement('div')
-  returned.id = 'card'
-  returned.card = card
+
+  var returned = document.createDocumentFragment()
+
+  var div = document.createElement('div')
+  returned.appendChild(div)
+  div.id = 'card'
+  div.card = card
+
+  card.mount(div)
 
   var errors = document.createElement('p')
   returned.appendChild(errors)
   errors.id = 'card-errors'
-
-  card.mount(returned)
 
   return returned
 }
