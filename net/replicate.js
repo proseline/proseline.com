95c7a573c3398d4cc661a4396b8ee1106ec4a0b3
Kyle E. Mitchell <kyle@kemitchell.com>
2018-07-18 02:59:25 -0700

    Do not expect callbacks from replication events

--- net/replicate.js
+++ net/replicate.js
@@ -21,7 +21,7 @@ module.exports = function (options) {
   // just received from it.
   var requestedFromPeer = []
 
-  protocol.once('handshake', function (callback) {
+  protocol.once('handshake', function () {
     database.createOfferStream()
       .pipe(flushWriteStream.obj(function (chunk, _, done) {
         var requestIndex = requestedFromPeer
@@ -32,43 +32,41 @@ module.exports = function (options) {
             )
           })
         if (requestIndex === -1) {
-          protocol.offer(chunk, done)
-        } else {
-          requestedFromPeer.splice(requestIndex, 1)
-          done()
+          return protocol.offer(chunk, done)
         }
+        requestedFromPeer.splice(requestIndex, 1)
+        done()
       }))
-    callback()
   })
 
   // When our peer requests an envelope...
-  protocol.on('request', function (request, callback) {
+  protocol.on('request', function (request) {
     debug('requested: %o', request)
     var publicKey = request.publicKey
     var index = request.index
     database.getEnvelope(publicKey, index, function (error, envelope) {
-      if (error) return callback(error)
-      if (envelope === undefined) return callback()
-      protocol.envelope(envelope, callback)
+      if (error) return debug(error)
+      if (envelope === undefined) return
+      protocol.envelope(envelope)
     })
   })
 
   // TODO: Prevent duplicate requests for the same envelope.
 
   // When our peer offers envelopes...
-  protocol.on('offer', function (offer, callback) {
+  protocol.on('offer', function (offer) {
     debug('offered: %o', offer)
     var publicKey = offer.publicKey
     var offeredIndex = offer.index
     database.getLogHead(publicKey, function (error, head) {
-      if (error) return callback(error)
+      if (error) return debug(error)
       if (head === undefined) head = -1
       var index = head + 1
       requestNextEnvelope()
       function requestNextEnvelope () {
-        if (index > offeredIndex) return callback()
+        if (index > offeredIndex) return
         protocol.request({publicKey, index}, function (error) {
-          if (error) return callback(error)
+          if (error) return debug(error)
           requestedFromPeer.push({publicKey, index})
           index++
           requestNextEnvelope()
@@ -78,25 +76,24 @@ module.exports = function (options) {
   })
 
   // When our peer sends an envelope...
-  protocol.on('envelope', function (envelope, callback) {
+  protocol.on('envelope', function (envelope) {
     debug('sent envelope: %o', envelope)
     // Validate envelope schema and signature.
     if (!validate.envelope(envelope)) {
-      debug('invalid envelope')
-      return callback()
+      return debug('invalid envelope')
     }
     // Validate body.
     if (!validate.body(envelope.message.body)) {
-      debug('invalid body')
-      return callback()
+      return debug('invalid body')
     }
     // Ensure body is for this project.
     if (envelope.message.project !== discoveryKey) {
-      debug('project mismatch')
-      return callback()
+      return debug('project mismatch')
     }
     // Write to our database.
-    database.putEnvelope(envelope, callback)
+    database.putEnvelope(envelope, function (error) {
+      if (error) return debug(error)
+    })
     // Call back about the update.
     onUpdate(envelope.project)
   })
